23363da9e09576ce0afff2109a4df8f32f607d23
Samfaria2002 <102630839+Samfaria2002@users.noreply.github.com>
2022-05-02 15:54:20 -0300

    Add files via upload

--- PinosAdapter/TestePinoQuadradoAdapter.java
+++ PinosAdapter/TestePinoQuadradoAdapter.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class TestePinoQuadradoAdapter {
     
     public static void main(String[] args) {
-        // Round fits round, no surprise.
+        //Definindo objetos e fazendo a entrada de dados
         BuracoRedondo buracoRedond = new BuracoRedondo(5);
         PinoRedondo pinoRedond = new PinoRedondo(5);
         PinoQuadrado pinoQuad1 = new PinoQuadrado(2);
         PinoQuadrado pinoQuad2 = new PinoQuadrado(20);
-        // hole.fits(smallSqPeg); // Won't compile.s
-    
-        // Adapter solves the problem.
+        
+        //Puxando a conversão
         PinoQuadradoAdapter smallSqPegAdapter = new PinoQuadradoAdapter(pinoQuad1);
         PinoQuadradoAdapter largeSqPegAdapter = new PinoQuadradoAdapter(pinoQuad2);
+
+        //Condicional de tamanho do encaixe
         if (buracoRedond.encaixa(smallSqPegAdapter)) {
             System.out.println("O pino quadrado encaixa no buraco redondo.");
         }
